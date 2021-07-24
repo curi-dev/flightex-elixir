fdcbe6ed6a07cba4de222ab5a47834da9345e85c
Tiago Lindgren Curi <shopper.tiago@gmail.com>
2021-07-24 17:43:55 -0300

    iniciando o exercício

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -1,9 +1,39 @@
 defmodule Flightex.Bookings.Booking do
   @keys [:complete_date, :local_origin, :local_destination, :user_id, :id]
+  @now NaiveDateTime.local_now()
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(
+        # it will come in a valid format
+        complete_date: date_string,
+        local_origin: local_origin,
+        local_destination: local_destination,
+        user_id: user_id
+      ) do
+    case verify_date(date_string) do
+      :valid ->
+        {:ok,
+         %__MODULE__{
+           complete_date: date_string,
+           local_origin: local_origin,
+           local_destination: local_destination,
+           user_id: user_id,
+           id: 0
+         }}
+
+      :invalid ->
+        {:error, "Please choose a valid date/time"}
+    end
+  end
+
+  def verify_date(date_string) do
+    range = NaiveDateTime.diff(date_string, @now, :second)
+
+    if range >= 60 do
+      :valid
+    else
+      :invalid
+    end
   end
 end
--- lib/users/user.ex
+++ lib/users/user.ex
@@ -3,7 +3,17 @@ defmodule Flightex.Users.User do
   @enforce_keys @keys
   defstruct @keys
 
+  def build(name, email, cpf, id) when is_bitstring(cpf) do
+    {:ok,
+     %__MODULE__{
+       name: name,
+       email: email,
+       cpf: cpf,
+       id: id
+     }}
+  end
+
   def build do
-    # TO DO
+    {:error, "Cpf must be a String"}
   end
 end
